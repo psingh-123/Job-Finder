7cd24a59cef1113a8dba1f9d8416470e443305de
Prashant Singh <123cs0070@iiitk.ac.in>
2025-11-17 15:19:11 +0530

    Update API endpoint for fetching analytics data

--- client/src/pages/AdminAnalytics.jsx
+++ client/src/pages/AdminAnalytics.jsx
@@ -19,7 +19,7 @@ const AdminAnalytics = () => {
   useEffect(() => {
     const fetchAnalytics = async () => {
       try {
-        const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/admin/analytics`, {
+        const res = await axios.get(`https://naukari-dhundo.onrender.com/api/admin/analytics`, {
           withCredentials: true,
         });
         console.log("Analytics Data:", res.data);
